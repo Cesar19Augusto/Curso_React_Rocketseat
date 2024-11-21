a3aafd4a1f2360913f450c2fddbfdee4c9c40313
Cesar Augusto <augustocesar875@gmail.com>
2024-11-21 19:26:17 -0300

    Feature adjustment v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import { SideBar } from './components/Sidebar.jsx';
 import { Header } from './components/Header.jsx';
 import { Post } from './components/Post.jsx';
@@ -54,6 +54,7 @@ export function App() {
         {posts.map(post => {
           return (
             <Post
+              key={post.id}
               author={post.author} 
               content={post.content} 
               publishedAt={post.publishedAt}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,14 +6,15 @@ import styles from './Post.module.css';
 import { useState } from 'react';
 
 
-export function Post ( { author, publishedAt, content } ) {
+export function Post ({ author, publishedAt, content }) {
 //Nescessario usar o estado do ract para atualizar com o novo comentario
 //Estado = Variáveis que eu quero que o componente monitore
 //Já que o useState retorno também um aray e uma função foi usado o conceito de desestruturação 
     const [comments, setComments] = useState ([
-        1,
-        2,
+        'Post muito bacana, hein?!'
     ])
+
+    const [newCommentText, setNewCommentText] = useState ('')
 
     const publishedDateFromatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -28,7 +29,12 @@ export function Post ( { author, publishedAt, content } ) {
         event.preventDefault()
 //Foi passado agora pra dentro dessa função qual é o novo valor do estado de comentarios ou sejá eu não passo só oque eu quero
 //eu passo um novo valor, isso é imutabilidade         
-        setComments([...comments, comments.length + 1]);
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value);
     }
 
     return (
@@ -43,25 +49,36 @@ export function Post ( { author, publishedAt, content } ) {
                 </div>
                 <time title={publishedDateFromatted} dateTime={publishedAt.toISOString()}> {publishedDateRelativeToNow} </time>
             </header> 
+
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type === 'paragraph') {
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     } else if (line.type === 'link') {
-                        return <p><a href="#">{line.content}</a></p>;
+                        return <p key={line.content}><a href="#">{line.content}</a></p>;
                     }
                 })}
             </div>
+
             <form onSubmit={handleCreateNewComment} className={styles.commetForm}>
+
                 <strong>Deixe seu feedback</strong>
-                <textarea placeholder="Deixe seu comentário"/>
+
+                <textarea 
+                    name="comment" 
+                    placeholder="Deixe seu comentário" 
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+
+                />
+
                 <footer>
                     <button type='submit'>Publicar</button>
                 </footer>
             </form>
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment key={comment} content={comment}/>
                 })}
             </div>
         </article>
