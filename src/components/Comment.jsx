ee94b3bc19d0b539bef00d506da0b7e8c3a62ee0
Cesar Augusto <augustocesar875@gmail.com>
2024-11-25 17:40:42 -0300

    add functions to Applaud from the comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
-export function Comment( {content} ) {
+export function Comment( {content, onDeleteComment} ) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment(){
+        setLikeCount((state) => {
+            return state +1
+        });
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/79229452?v=4"/>
@@ -13,15 +26,16 @@ export function Comment( {content} ) {
                             <strong>Diego Fernandes</strong>
                             <time title='11 de maio às 08:13h' dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
                         </div>
-                        <button title="Deletar comentário"> 
+                        <button onClick={handleDeleteComment} title="Deletar comentário"> 
                         <Trash size={24} />
                         </button>
                     </header>
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
-                      <ThumbsUp /> Aplaudir <span>20</span>
+                    <button onClick={handleLikeComment}>
+                      <ThumbsUp /> 
+                      Aplaudir <span>{likeCount}</span>
                     </button>  
                 </footer>
             </div>
