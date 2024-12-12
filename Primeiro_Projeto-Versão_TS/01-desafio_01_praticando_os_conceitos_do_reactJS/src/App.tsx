bd3b0a4a3fcfc4f53f499748a2650f3a6f4d2a09
Cesar Augusto <augustocesar875@gmail.com>
2024-12-12 17:03:45 -0300

    atualização

---
+++ Primeiro_Projeto-Versão_TS/01-desafio_01_praticando_os_conceitos_do_reactJS/src/App.tsx
@@ -0,0 +1,19 @@
+import { Header } from './components/Header';
+import './App.css';
+import './global.css';
+
+function App() {
+  return (
+    <div>
+      {/*Precisa da header*/}
+      {/*Precisa da addingTask que esta dentro da header*/}
+      <Header />
+      <main>
+        {/*Precisa da taskBar*/}
+        {/*Precisa da task*/}
+      </main>
+    </div>
+  )
+}
+
+export default App
